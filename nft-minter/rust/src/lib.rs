738b0675e2de3f61572e659a4380ccaa7bd3355d
Alex Smith <alexander.richard.smith@gmail.com>
2022-02-24 17:08:10 -0500

    Borsch SerDe is workingts-node main.tsts-node main.ts

--- nft-minter/rust/src/lib.rs
+++ nft-minter/rust/src/lib.rs
@@ -1,29 +1,6 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{
-    account_info::{next_account_info, AccountInfo},
-    entrypoint,
-    entrypoint::ProgramResult,
-    msg,
-    program_error::ProgramError,
-    pubkey::Pubkey,
-};
+// pub mod instruction;
+pub mod processor;
+pub mod error;
 
-// Declare and export the program's entrypoint
-entrypoint!(process_instruction);
-
-// Program entrypoint's implementation
-pub fn process_instruction(
-    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
-    accounts: &[AccountInfo], // The account to say hello to
-    _instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
-) -> ProgramResult {
-
-    msg!("TOKR nft-minter Rust program entrypoint");
-    // Iterating accounts is safer than indexing
-    let accounts_iter = &mut accounts.iter();
-
-    let account = next_account_info(accounts_iter)?;
-    msg!("Account! {}, program! {} ", account.owner, program_id);
-
-    Ok(())
-}
+#[cfg(not(feature = "no-entrypoint"))]
+pub mod entrypoint;
