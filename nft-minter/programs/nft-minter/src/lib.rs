32a46693abe8d46c6cf03a9f8034e8d735bd7e88
Alex Smith <alexander.richard.smith@gmail.com>
2022-02-28 13:26:58 -0500

    nft minter

--- nft-minter/programs/nft-minter/src/lib.rs
+++ nft-minter/programs/nft-minter/src/lib.rs
@@ -12,4 +12,12 @@ pub mod nft_minter {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init, // 1. Hey Anchor, initialize an account with these details for me
+        payer = payer, // 2. See that authority Signer (pubkey) down there? They're paying for this
+        space = 8 // 3.A) all accounts need 8 bytes for the account discriminator prepended to the account
+        + 32 // 3.B) authority: Pubkey needs 32 bytes
+    )]
+    pub payer: Account<'info>
+}
